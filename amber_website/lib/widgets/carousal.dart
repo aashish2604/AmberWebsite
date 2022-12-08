ad22bdb0d0872bd34b3d5b24abe8824bb981d323
aashish2604 <aashish.260401@gmail.com>
2022-12-08 16:21:59 +0530

    Added carousal

---
+++ amber_website/lib/widgets/carousal.dart
@@ -0,0 +1,29 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SlidingCarousal extends StatelessWidget {
+  const SlidingCarousal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    List<String> images = [
+      'images/amber_dp.jpg',
+      'images/card_background.jpg',
+      'images/amber_dp.jpg',
+      'images/amber_dp.jpg'
+    ];
+
+    return CarouselSlider(
+      options: CarouselOptions(height: height * 0.8, autoPlay: true),
+      items: images.map((e) {
+        return Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(e), fit: BoxFit.fill)),
+        );
+      }).toList(),
+    );
+  }
+}
